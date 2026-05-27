834ecea677f7c323bd9d57e82af1527680994479
joao <sonicjoa06@gmail.com>
2026-05-27 16:28:10 -0300

    Criando a Pasta do projeto e adicionando um aquivo

---
+++ InterFace/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InterFace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
